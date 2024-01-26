7dbcea9a58d4104bfe0d181f5c4e56cfa5c7df0b
TheOnlyBailey <baileybrown2601@gmail.com>
2024-01-26 21:01:28 +0000

    Tiles Fixed?

--- src/player.cs
+++ src/player.cs
@@ -4,66 +4,66 @@ using System.Diagnostics.Contracts;
 
 public partial class player : CharacterBody2D
 {
-    public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
 
-    // Get the gravity from the project settings to be synced with RigidBody nodes.
-    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector2 velocity = Velocity;
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
 
-        // Add the gravity.
-        if (!IsOnFloor())
-            velocity.Y += gravity * (float)delta;
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
 
-        // Handle Jump.
-        if (Input.IsActionJustPressed("Jump") && IsOnFloor())
-            velocity.Y = JumpVelocity;
+		// Handle Jump.
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
 
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-        {
-            velocity.X = direction.X * Speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-        }
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 
    
 
-    [Export]
-    public int MaxHp = 10;
-    [Export]
-    private int Hp = MaxHp;
-    [Export]
-    public int Damage = 10;
-    [signal]
-    public delegate void MySignalEventHandler();
+	[Export]
+	public int MaxHp = 10;
+	[Export]
+	private int Hp = MaxHp;
+	[Export]
+	public int Damage = 10;
+	[signal]
+	public delegate void MySignalEventHandler();
 
-    public int DamageTaken ()
-    {
-        return HpChanged(DamageAmount, Hp);
-    
-    }
+	public int DamageTaken ()
+	{
+		return HpChanged(DamageAmount, Hp);
+	
+	}
 
-    
-    private int HpChanged (int Change)
-    {
-        int Total = Hp - Change;
-        EmitSignal(MySignalEventHandler, Hp);
-        return Total;
+	
+	private int HpChanged (int Change)
+	{
+		int Total = Hp - Change;
+		EmitSignal(MySignalEventHandler, Hp);
+		return Total;
 
 
-    }
+	}
 
 
 }
