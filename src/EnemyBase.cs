834357ed04859cb9a56c9c30bd19ed5dcea4ec60
JamieLAtkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2024-01-26 20:57:48 +0000

    updated enemybase.cs

--- src/EnemyBase.cs
+++ src/EnemyBase.cs
@@ -1,30 +1,25 @@
 using Godot;
 using System;
 
-public partial class enemyBase : CharacterBody2D
+public partial class EnemyBase : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-	public const Vector2 FLOOR_NORMAL = Vector2.up;
-	private Vector2 _velocity = new Vector2();
-	private Raycast2D raycast;
+	public Vector2 FLOOR_NORMAL = new Vector2(0.0f,-1.0f);
+	private RayCast2D raycast;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _OnReady()
+	public override void _Ready()
 	{
-		raycast = GetNode("RayCast2D");
+		raycast = (RayCast2D)GetNode("RayCast2D");
+		Velocity = new Vector2(100,0);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		_velocity.Y += gravity * delta;
-		_velocity.Y = MoveAndSlide(_velocity, FLOOR_NORMAL).Y;
-
-		if (IsOnWall() or (IsOnFloor() && !raycast.IsColliding()){
-			_velocity.X *= -1.0;
-			raycast.position.X *= -1.0;
-		}
+		Velocity = new Vector2(10,10);
+		MoveAndSlide();
 	}
 }
