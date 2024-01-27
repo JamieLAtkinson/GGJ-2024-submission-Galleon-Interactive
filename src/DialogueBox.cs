08e50d226dc6759d4a406894e92235210336f860
JamieLAtkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2024-01-27 04:41:18 +0000

    added writable dialogue box

---
+++ src/DialogueBox.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class DialogueBox : Control
+{
+	private RichTextLabel _label;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_label = GetNode<RichTextLabel>("TextureRect/RichTextLabel")	
+	}
+	public void AppendText(string Text){
+		_label.Text += Text + "\n"
+	}
+}
